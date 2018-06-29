d3735b290ccc96af107d5cbdddb10a1e7979be35
princeallan <allankirui57@gmail.com>
2018-06-29 09:33:35 +0300

    basics addedd

---
+++ src/components/employeesList.vue
@@ -0,0 +1,47 @@
+<template>
+<div id="app">
+
+
+    </div>
+
+</template>
+<script>
+    export default {
+        name: 'app',
+        data() {
+            return {
+                user: {
+                    message: '',
+                    firsname: '',
+                    lastname: '',
+
+                },
+                users: []
+
+            }
+
+        }
+    }
+</script>
+
+<style>
+    table {
+        font-family: "Helvetica Neue", Helvetica, Roboto, Arial, sans-serif;
+        border-collapse: collapse;
+        width: 80%;
+    }
+
+    td, th {
+        border: 1px solid #dddddd;
+        text-align: left;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #008080;
+    }
+
+    .greebody{
+        background: green;
+    }
+    </style>
